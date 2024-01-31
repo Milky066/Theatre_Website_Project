d07c84c112ea6ff07b61553d5ea9704725dbcbe8
Milky066 <sahassapol@gmail.com>
2024-02-01 00:05:53 +0800

    Last commit

--- Backend/checkLogin.php
+++ Backend/checkLogin.php
@@ -2,6 +2,7 @@
 session_start();
 $user_id = null;
 if (isset($_SESSION["user_id"])) {
-    $user_id = $_SESSION["user_id"][0];
+    // $user_id = $_SESSION["user_id"][0];
+    $user_id = $_SESSION["user_id"];
     echo "<script>console.log('User ID: '+$user_id)</script>";
 }
